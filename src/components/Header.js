c7f389a9b3538b3be78238b147415d46c771f5c9
Jessy The <jessy@brightfish.nl>
2021-02-19 17:26:47 +0100

    CHNGE: Add Styled Components to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Header.css";
+import styled from "styled-components";
 import IconButton from "@material-ui/core/IconButton";
 import PinterestIcon from "@material-ui/icons/Pinterest";
 import SearchIcon from "@material-ui/icons/Search";
@@ -17,20 +17,20 @@ function Header(props) {
   };
 
   return (
-    <div className="header">
-      <div className="header__logo">
+    <Wrapper>
+      <LogoWrapper>
         <IconButton>
           <PinterestIcon />
         </IconButton>
-      </div>
-      <div className="header__button homePage">
+      </LogoWrapper>
+      <HomePageButton>
         <a href="/">Homepage</a>
-      </div>
-      <div className="header__button following">
+      </HomePageButton>
+      <FollowingButton>
         <a href="/">Following</a>
-      </div>
-      <div className="header__search">
-        <div className="header__searchContainer">
+      </FollowingButton>
+      <SearchWrapper>
+        <SearchBarWrapper>
           <IconButton>
             <SearchIcon />
           </IconButton>
@@ -40,9 +40,9 @@ function Header(props) {
               {/*CAN BE LEFT EMPTY*/}
             </button>
           </form>
-        </div>
-      </div>
-      <div className="header__menuItems">
+        </SearchBarWrapper>
+      </SearchWrapper>
+      <IconsWrapper>
         <IconButton>
           <NotificationsIcon />
         </IconButton>
@@ -55,8 +55,108 @@ function Header(props) {
         <IconButton>
           <KeyboardArrowDownIcon />
         </IconButton>
-      </div>
-    </div>
+      </IconsWrapper>
+    </Wrapper>
   );
 }
 export default Header;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+  padding: 12px 4px 4px 16px;
+  background-color: white;
+  color: black;
+`;
+
+const LogoWrapper = styled.div`
+  .MuiSvgIcon-root {
+    color: #e60023;
+    font-size: 32px;
+    cursor: pointer;
+  }
+`;
+
+const HomePageButton = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: rgb(17, 17, 17);
+
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: 700;
+  }
+`;
+
+const FollowingButton = styled.div`
+  display: flex;
+  height: 48px;
+  min-width: 123px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: white;
+
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: 700;
+  }
+
+  :hover {
+    background-color: #e1e1e1;
+  }
+`;
+
+const SearchWrapper = styled.div`
+  flex: 1;
+`;
+
+const SearchBarWrapper = styled.div`
+  background-color: #efefef;
+  display: flex;
+  height: 48px;
+  width: 100%;
+  border-radius: 50px;
+  border: none;
+  padding-left: 10px;
+
+  form {
+    display: flex;
+    flex: 1;
+  }
+
+  form > input {
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    margin-left: 5px;
+    font-size: 16px;
+  }
+
+  form > button {
+    display: none;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input:active {
+    outline: none;
+  }
+
+  input:hover {
+    outline: none;
+  }
+`;
+
+const IconsWrapper = styled.div``;
